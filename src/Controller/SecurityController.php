0bae2400aec1f5e8c6a8829b2f78bf5f726886cc
azizbaffoun <azizbaffoun67@gmail.com>
2025-04-15 02:40:00 +0100

    user management

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,26 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Form\FormFactoryInterface;
 
 class SecurityController extends AbstractController
 {
+    #[Route(path: '/', name: 'app_root')]
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, FormFactoryInterface $formFactory): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+            if ($this->isGranted('ROLE_ADMIN')) {
+                return $this->redirectToRoute('admin_dashboard');
+            }
+            return $this->redirectToRoute('user_dashboard');
+        }
 
-        // get the login error if there is one
+        $user = new User();
+        $registrationForm = $formFactory->create(RegistrationFormType::class, $user);
+
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+            'registrationForm' => $registrationForm->createView(),
+        ]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
