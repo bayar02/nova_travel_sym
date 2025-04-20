79de365bf65a68f7ae27e40476b1eb429c36d034
goatrider69 <zizou.bayar@gmail.com>
2025-04-20 01:20:54 +0100

    control saisie console

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,16 +4,13 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class RegistrationFormType extends AbstractType
 {
@@ -22,49 +19,56 @@ class RegistrationFormType extends AbstractType
         $builder
             ->add('prenom', TextType::class, [
                 'constraints' => [
-                    new NotBlank(['message' => 'Please enter your first name'])
+                    new Assert\NotBlank(['message' => 'Veuillez entrer votre prénom']),
+                    new Assert\Length(['min' => 2, 'minMessage' => 'Le prénom doit contenir au moins {{ limit }} caractères']),
                 ]
             ])
             ->add('nom', TextType::class, [
                 'constraints' => [
-                    new NotBlank(['message' => 'Please enter your last name'])
+                    new Assert\NotBlank(['message' => 'Veuillez entrer votre nom']),
+                    new Assert\Length(['min' => 2, 'minMessage' => 'Le nom doit contenir au moins {{ limit }} caractères']),
                 ]
             ])
             ->add('cin', TextType::class, [
                 'constraints' => [
-                    new NotBlank(['message' => 'Please enter your CIN'])
-                    // Add Length or Regex constraint for CIN format if needed
+                    new Assert\NotBlank(['message' => 'Veuillez entrer votre CIN']),
+                    new Assert\Regex([
+                        'pattern' => '/^\d{8}$/',
+                        'message' => 'Le CIN doit contenir exactement 8 chiffres',
+                    ])
                 ]
             ])
             ->add('tel', TelType::class, [
                 'constraints' => [
-                    new NotBlank(['message' => 'Please enter your telephone number'])
-                    // Add Length or Regex constraint for Tel format if needed
+                    new Assert\NotBlank(['message' => 'Veuillez entrer votre numéro de téléphone']),
+                    new Assert\Regex([
+                        'pattern' => '/^\d{8}$/',
+                        'message' => 'Le numéro de téléphone doit contenir exactement 8 chiffres',
+                    ])
                 ]
             ])
-            ->add('mail', EmailType::class, [ // Use EmailType for validation
+            ->add('mail', EmailType::class, [
                 'constraints' => [
-                    new NotBlank(['message' => 'Please enter your email'])
+                    new Assert\NotBlank(['message' => 'Veuillez entrer votre adresse email']),
+                    new Assert\Email(['message' => 'Adresse email invalide']),
                 ]
             ])
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
+                    new Assert\NotBlank(['message' => 'Veuillez entrer un mot de passe']),
+                    new Assert\Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
+                        'minMessage' => 'Le mot de passe doit contenir au moins {{ limit }} caractères',
                         'max' => 4096,
                     ]),
+                    new Assert\Regex([
+                        'pattern' => '/(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&\-_#])/',
+                        'message' => 'Le mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre et un caractère spécial',
+                    ])
                 ],
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
