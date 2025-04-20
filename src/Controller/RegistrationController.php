6f0107a0ef2a246e349ee0f62f2b98115789da8f
goatrider69 <zizou.bayar@gmail.com>
2025-04-20 01:20:54 +0100

    control saisie console

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -43,7 +43,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('aziz.bayar@esprit.tn', 'SiBaffoun'))
+                    ->from(new Address('aziz.bayar@esprit.tn', 'bayar'))
                     ->to((string) $user->getMail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
